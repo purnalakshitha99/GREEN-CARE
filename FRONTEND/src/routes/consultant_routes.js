8b62b119c6979a1e4cccf0a68becdd62aecfb67e
Nipun Ganepola <85781012+Nipun44@users.noreply.github.com>
2023-04-15 00:13:51 +0530

    appointment and news backend

--- FRONTEND/src/routes/consultant_routes.js
+++ FRONTEND/src/routes/consultant_routes.js
@@ -1,8 +1,10 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import CreateAppointment from "../views/consultant/createAppoinments";
-import AddNews from "..views/consultant/AddNews";
+
 import TestT from "../views/consultant/test";
+
+
 
 import Side_nav from "../layouts/side_nav";
 
@@ -10,10 +12,12 @@ export default function consultant_routes() {
   return (
     <Router>
       <Routes>
+      
+        
         <Route path="/createAppointment" exact element={<CreateAppointment />}></Route>
         <Route path="/test" exact element={<TestT />}></Route>
-        <Route path="/addNews" exact element={<AddNews />}></Route>
-        <Route path="/test" exact element={<TestT />}></Route>
+      
+     
        
       </Routes>
     </Router>
