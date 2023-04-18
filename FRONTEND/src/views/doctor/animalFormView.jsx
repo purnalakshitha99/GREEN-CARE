9bffabf87cd004e769aabcf16832371b4669ac77
sanda-ni <it21172632@my.sliit.lk>
2023-04-18 21:49:17 +0530

    minor changes

--- FRONTEND/src/views/doctor/animalFormView.jsx
+++ FRONTEND/src/views/doctor/animalFormView.jsx
@@ -93,121 +93,166 @@ const AnimalForm = () => {
     setErrors({});
   };
   return (
-    <section id="learnMore" className="hero-block">
+ <section id="learnMore" className="hero-block">
       <header id="header">
         <AppHeader />
       </header>
-
-      <div class="container">
-        <form onSubmit={handleSubmit}>
-          <section class="get-in-touch">
-            <h1 class="title">Request Veterinary Advice</h1>
-            
-            <label htmlFor="firstName">First Name </label>
-            <div class="myDiv">
-            <input
+      
+      {/* <div class="container"></div> */}
+      <div class="container2">
+      <h2>Responsive Form</h2>
+   <form onSubmit={handleSubmit}>
+  <div class="row">
+    <div class="col-25">
+      <label for="fname">First Name</label>
+    </div>
+    <div class="col-75">
+       <input
               type="text"
               name="firstName"
               value={formData.firstName}
               onChange={handleChange}
-            /></div>
+            />
             {errors.firstName && <span>{errors.firstName}</span>}
-
-
-            <label htmlFor="lastName">Last Name</label>
-            <div class="myDiv">
-            <input
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-25">
+      <label for="lname">Last Name</label>
+    </div>
+    <div class="col-75">
+      <input
               type="text"
               name="lastName"
               value={formData.lastName}
               onChange={handleChange}
             />
             {errors.lastName && <span>{errors.lastName}</span>}
-            </div>
-            <label htmlFor="email">Email</label>
-            <input
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-25">
+     <label htmlFor="email">Email</label>
+    </div>
+    <div class="col-75">
+     <input
               type="text"
               name="email"
               value={formData.email}
               onChange={handleChange}
             />
             {errors.email && <span>{errors.email}</span>}
-
-            <label htmlFor="address">Address</label>
-            <input
-              type="text"
-              name="address"
-              value={formData.address}
-              onChange={handleChange}
-            />
-            {errors.address && <span>{errors.address}</span>}
-
-            <label htmlFor="phoneNumber">Phone Number </label>
-            <input
+    </div>
+  </div>
+  
+   <div class="row">
+    <div class="col-25">
+      <label htmlFor="phoneNumber">Phone Number</label>
+    </div>
+    <div class="col-75">
+      <input
               type="tel"
               name="phoneNumber"
               value={formData.phoneNumber}
               onChange={handleChange}
             />
             {errors.phoneNumber && <span>{errors.phoneNumber}</span>}
-
-            <label htmlFor="subject">Subject</label>
-            <input
+    </div>
+  </div>
+   <div class="row">
+    <div class="col-25">
+      <label htmlFor="address">Address</label>
+    </div>
+    <div class="col-75">
+      <input
               type="text"
-              name="subject"
-              value={formData.subject}
+              name="address"
+              value={formData.address}
               onChange={handleChange}
             />
-            {errors.subject && <span>{errors.subject}</span>}
+            {errors.address && <span>{errors.address}</span>}
+    </div>
+  </div>
 
-            <label htmlFor="animalSpecies">Animal Species</label>
-            <input
+  
+  <div class="row">
+    <div class="col-25">
+      <label htmlFor="age">Age</label>
+    </div>
+    <div class="col-75">
+     <input
               type="text"
-              name="animalSpecies"
-              value={formData.animalSpecies}
-              onChange={handleChange}
-            />
-            {errors.animalSpecies && <span>{errors.animalSpecies}</span>}
-
-            <label htmlFor="weight">Weight</label>
-            <input
-              type="number"
-              name="weight"
-              value={formData.weight}
-              onChange={handleChange}
-            />
-            {errors.weight && <span>{errors.weight}</span>}
-
-            <label htmlFor="age">Age</label>
-            <input
-              type="number"
               name="age"
               value={formData.age}
               onChange={handleChange}
             />
             {errors.age && <span>{errors.age}</span>}
-
-            <label htmlFor="attachment">Attachment</label>
-            <input type="file" name="attachment" onChange={handleFileChange} />
-
-            <label htmlFor="description">Description</label>
-            <textarea
-              name="description"
-              value={formData.description}
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-25">
+      <label htmlFor="weight">Weight</label>
+    </div>
+    <div class="col-75">
+     <input
+              type="text"
+              name="weight"
+              value={formData.weight}
               onChange={handleChange}
-            ></textarea>
+            />
+            {errors.weight && <span>{errors.weight}</span>}
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-25">
+       <label htmlFor="animalSpecies">Animal Species</label>
+    </div>
+    <div class="col-75">
+      <select id="animal" name="animal">
+        <option value="cow">Cow</option>
+        <option value="horse">Horse</option>
+        <option value="goat">Goat</option>
+        <option value="pig">Pig</option>
+        <option value="chicken">Chicken</option>
+        <option value="sheep">Sheep</option>
+      </select>
+    </div>
+    <div class="row">
+    <div class="col-25">
+      <label for="attachment">file attachement</label>
+    </div>
+    <div class="col-75">
+       <input
+       
+              type="file"
+              name="attachment"
+              value={formData.attachment}
+              onChange={handleChange}
+            />
+            {errors.attachment && <span>{errors.attachment}</span>}
+    </div>
+  </div>
+  </div>
+  <div class="row">
+    <div class="col-25">
+      <label for="subject">Help</label>
+    </div>
+    <div class="col-75">
+      <textarea id="subject" name="subject" placeholder="Write something.."></textarea>
+    </div>
+  </div>
+  <br/>
+  <div class="row">
+    <div class="button">
+    <button type="submit" class="btn btn-primary">Submit</button>
+    <button type="clear" class="btn btn-primary" onClick={handleClear}>Clear</button>
+    </div>
+  </div>
+  </form>
+</div>
 
-            <div>
-              <button type="submit">Submit</button>&ensp;&ensp;&ensp;
-              <button type="button" onClick={handleClear}>
-                {" "}
-                Clear&ensp;&ensp;
-              </button>
-            </div>
-          </section>
-        </form>
-      </div>
     </section>
+    
   );
 };
 
