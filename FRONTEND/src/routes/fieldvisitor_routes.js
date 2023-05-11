cd580e31eb22085fa1924b87f6773a0efd501d41
IT21158254 <IT21158254@my.sliit.lk>
2023-05-11 23:20:05 +0530

    work

--- FRONTEND/src/routes/fieldvisitor_routes.js
+++ FRONTEND/src/routes/fieldvisitor_routes.js
@@ -1,16 +1,14 @@
-
 import React from "react";
 import Home from "../views/feild_visitor/component/Home";
 import InsertFarmerInformation from "../views/feild_visitor/component/InsertFarmerInformation";
 import InformationTable from "../views/feild_visitor/component/InformationTable";
-import PageNotFound from "../views/feild_visitor/component/PageNotFound";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import FieldReport from "../views/feild_visitor/component/FieldReport";
 import { BrowserRouter } from "react-router-dom";
 import ReporLlist from "../views/feild_visitor/component/ReporLlist";
 import Report from "../views/feild_visitor/component/Report";
 import EditRport from "../views/feild_visitor/component/EditRport";
-import AjHome from "../views/aj/AjHome"
+import AjHome from "../views/aj/AjHome";
 import DamageReport from "../views/feild_visitor/component/DamageReport";
 
 function fieldvisitor_routes() {
@@ -18,7 +16,6 @@ function fieldvisitor_routes() {
     <div>
       <Router>
         <Routes>
-          
           <Route path="/" exact element={<AjHome />}></Route>
           <Route path="/visitorhome" exact element={<Home />}></Route>
           <Route path="/infotable" exact element={<InformationTable />}></Route>
@@ -30,7 +27,6 @@ function fieldvisitor_routes() {
           <Route path="/fieldreport" exact element={<FieldReport />}></Route>
           <Route path="/displayreport" exact element={<ReporLlist />}></Route>
           <Route path="/report" exact element={<Report />}></Route>
-          <Route path="*" exact element={<PageNotFound />}></Route>
           <Route path="/damagereport" exact element={<DamageReport />}></Route>
           <Route path="/editreport/:_id" exact element={<EditRport />}></Route>
         </Routes>
